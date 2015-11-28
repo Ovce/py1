396f28660613c1185d6fb80bc95f061650ffe0af
Ovce <PomerancovyPunta@seznam.cz>
2015-11-28 13:56:52 +0100

    init

---
+++ polls/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from polls.models import Poll
+#from django.contrib import admin
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+
+admin.site.register(Poll,PollAdmin)
